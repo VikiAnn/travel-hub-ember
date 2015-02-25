2a8f4249f746c93f9d18959e91e505145481dfb0
Herbert Joseph <techniquea2z@gmail.com>
2015-02-25 14:29:04 -0700

    Unit tested trip model

--- app/models/trip.js
+++ app/models/trip.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 let Trip = DS.Model.extend({
   name:       DS.attr('string'),
-  start_date: DS.attr("date"),
+  startDate: DS.attr("date"),
   users:      DS.hasMany('users', { async: true }),
-  end_date:   DS.attr("date")
+  endDate:   DS.attr("date")
 });
 
 Trip.reopenClass({
@@ -12,22 +12,22 @@ Trip.reopenClass({
     {
       "id": 1,
       "name": "Steve's Emacs Trip",
-      "start_date": "01/13/2013",
-      "end_date": "01/13/2014",
+      "startDate": "01/13/2013",
+      "endDate": "01/13/2014",
       "users": [2, 4]
     },
     {
       "id": 2,
       "name": "Jorge's Mexico Trip",
-      "start_date": "07/13/2015",
-      "end_date": "07/23/2015",
+      "startDate": "07/13/2015",
+      "endDate": "07/23/2015",
       "users": [1]
     },
     {
       "id": 3,
       "name": "Worace's Ski Trip",
-      "start_date": "02/13/2014",
-      "end_date": "03/1/2014",
+      "startDate": "02/13/2014",
+      "endDate": "03/1/2014",
       "users": [1, 3]
     }
   ]
