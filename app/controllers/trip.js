48bcaa9d391e72de34ab764a9fa557a7a5cac0ca
Viki Harrod <victoria.harrod@gmail.com>
2015-03-04 18:34:02 -0700

    Remove comment

--- app/controllers/trip.js
+++ app/controllers/trip.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  // needs: ['trips'],
+
   allPosts: function() {
     return this.get('content').map(function (user) {
       return user.posts;
     });
   }.property('content.[]')
+  
 });
